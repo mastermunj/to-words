7932af0dc75867b85d1562202eb7337e62f15ccd
Munjal Dhamecha <mastermunj@gmail.com>
2021-08-04 20:19:07 +0530

    chore: update dependencies

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   'package.json': 'sort-package-json',
-  '*.{ts,tsx}': 'eslint --max-warnings=0 . --ext=.ts --fix',
+  '*.{ts,tsx}': 'eslint --max-warnings=0 . --ext=.ts --fix --no-ignore',
   '**/*.ts?(x)': () => 'tsc -p tsconfig.json',
 };
